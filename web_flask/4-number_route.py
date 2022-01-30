cecc496ad89c1f160126791b07a82a0d8328065d
Mahalaksmy <3354@holbertonschool.com>
2022-01-30 18:58:14 +0100

    Task 4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Thii is a a script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def helloHBNB():
+    """Return text Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Return text HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Return the value of the text"""
+    txt = text.replace('_', ' ')
+    return 'C {}'.format(txt)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """Return the text """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Return a int"""
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000)
