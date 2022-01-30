be3aa7562f387f4800d316d2fa28c101e248216c
Mahalaksmy <3354@holbertonschool.com>
2022-01-30 18:16:36 +0100

    Task 2. C is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Thii is a a script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def helloHBNB():
+    """Return text Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Return text HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Return the value of the text"""
+    txt = text.replace('_', ' ')
+    return 'C {}'.format(txt)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000)
